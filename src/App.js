0065cdf74c773812c97fff9e4e5e855139ebd8dd
Towsif Ahmed Omi <towsif031@gmail.com>
2020-07-16 17:37:15 +0600

    onchange encode done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import Encode from './Encode';
 
 function App() {
 	const [stringData, setStringData] = useState({
-		inputString: 'Hello World!'
+		inputString: ''
 	});
+
+	function onChangeInput(e) {
+		setStringData({ inputString: e.target.value });
+	}
 
 	return (
 		<div className="App">
+			<form>
+				<input
+					type="text"
+					name="inputString"
+					placeholder="enter string..."
+					value={setStringData.inputString}
+					onChange={onChangeInput}
+				></input>
+			</form>
 			<Encode stringData={stringData} />
 		</div>
 	);

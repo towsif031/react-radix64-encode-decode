4e6c77c1b4fe0d3c36632de22e7938e39cbb2304
Towsif Ahmed Omi <towsif031@gmail.com>
2020-07-17 10:14:07 +0600

    Updates

---
+++ precache-manifest.bdb1629c6c338f683225a8d0a94b9cf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b580e4b24c5b3e89562e5ece8ccde0b8",
+    "url": "/react-radix64-ed/index.html"
+  },
+  {
+    "revision": "3cd4e408abaa5dde0bb8",
+    "url": "/react-radix64-ed/static/css/main.6ba19d0b.chunk.css"
+  },
+  {
+    "revision": "bb378dbc31c603479062",
+    "url": "/react-radix64-ed/static/js/2.bf06c785.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-radix64-ed/static/js/2.bf06c785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cd4e408abaa5dde0bb8",
+    "url": "/react-radix64-ed/static/js/main.8f56cbf6.chunk.js"
+  },
+  {
+    "revision": "2b215b4ae449274df7f3",
+    "url": "/react-radix64-ed/static/js/runtime-main.3352fd21.js"
+  }
+]);
